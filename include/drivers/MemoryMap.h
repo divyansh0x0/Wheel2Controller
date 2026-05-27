22751fad1c219c2cb479f3ca94fdc86095e60def
divyansh0x0 <divyansh0x0@gmail.com>
2026-05-27 14:26:55 +0530

    feat: add DMA memory map

--- include/drivers/MemoryMap.h
+++ include/drivers/MemoryMap.h
@@ -12,6 +12,7 @@
 
 namespace W2 {
     inline constexpr unsigned int CPU_FREQUENCY = 72'000'000;
+
     /**
      * @brief Determines the internal pull-up/pull-down resistor connection for an input pin.
      */
@@ -19,11 +20,12 @@ namespace W2 {
         Down = 0, ///< Configures input pin with internal pull-down resistor connected to VSS.
         Up = 1 ///< Configures input pin with internal pull-up resistor connected to VDD.
     };
+
     enum class BaudRate : unsigned int {
-        Baud9600   = 9600,
-        Baud19200  = 19200,
-        Baud38400  = 38400,
-        Baud57600  = 57600,
+        Baud9600 = 9600,
+        Baud19200 = 19200,
+        Baud38400 = 38400,
+        Baud57600 = 57600,
         Baud115200 = 115200,
         Baud230400 = 230400
     };
@@ -66,13 +68,17 @@ namespace W2 {
         SlowAlternatePushPull = 0b1010, ///< Alternate function output push-pull, max speed 2 MHz (CNF=10, MODE=10).
         SlowAlternateOpenDrain = 0b1110, ///< Alternate function output open-drain, max speed 2 MHz (CNF=11, MODE=10).
 
-        MediumGeneralPurposePushPull = 0b0001, ///< General purpose output push-pull, max speed 10 MHz (CNF=00, MODE=01).
-        MediumGeneralPurposeOpenDrain = 0b0101, ///< General purpose output open-drain, max speed 10 MHz (CNF=01, MODE=01).
+        MediumGeneralPurposePushPull = 0b0001,
+        ///< General purpose output push-pull, max speed 10 MHz (CNF=00, MODE=01).
+        MediumGeneralPurposeOpenDrain = 0b0101,
+        ///< General purpose output open-drain, max speed 10 MHz (CNF=01, MODE=01).
         MediumAlternatePushPull = 0b1001, ///< Alternate function output push-pull, max speed 10 MHz (CNF=10, MODE=01).
-        MediumAlternateOpenDrain = 0b1101, ///< Alternate function output open-drain, max speed 10 MHz (CNF=11, MODE=01).
+        MediumAlternateOpenDrain = 0b1101,
+        ///< Alternate function output open-drain, max speed 10 MHz (CNF=11, MODE=01).
 
         FastGeneralPurposePushPull = 0b0011, ///< General purpose output push-pull, max speed 50 MHz (CNF=00, MODE=11).
-        FastGeneralPurposeOpenDrain = 0b0111, ///< General purpose output open-drain, max speed 50 MHz (CNF=01, MODE=11).
+        FastGeneralPurposeOpenDrain = 0b0111,
+        ///< General purpose output open-drain, max speed 50 MHz (CNF=01, MODE=11).
         FastAlternatePushPull = 0b1011, ///< Alternate function output push-pull, max speed 50 MHz (CNF=10, MODE=11).
         FastAlternateOpenDrain = 0b1111, ///< Alternate function output open-drain, max speed 50 MHz (CNF=11, MODE=11).
     };
@@ -133,35 +139,36 @@ namespace W2 {
         void enablePWM(TimerChannel channel) {
             switch (channel) {
                 case TimerChannel::Channel1:
-                    this->CCMR1 &= ~(0b11 << 0);  // Configure channel 1 in output compare mode (CC1S = 00 in TIMx_CCMR1)
+                    this->CCMR1 &= ~(0b11 << 0); // Configure channel 1 in output compare mode (CC1S = 00 in TIMx_CCMR1)
                     this->CCMR1 &= ~(0b111 << 4); // Clear output compare 1 mode configuration bits (OC1M)
-                    this->CCMR1 |= (0b110 << 4);  // Set OC1M to PWM Mode 1 (0b110)
-                    this->CCMR1 |= (1 << 3);      // Enable Output Compare 1 Preload (OC1PE)
-                    this->CCER  |= (1 << 0);      // Enable Output Compare 1 output (CC1E in TIMx_CCER)
+                    this->CCMR1 |= (0b110 << 4); // Set OC1M to PWM Mode 1 (0b110)
+                    this->CCMR1 |= (1 << 3); // Enable Output Compare 1 Preload (OC1PE)
+                    this->CCER |= (1 << 0); // Enable Output Compare 1 output (CC1E in TIMx_CCER)
                     break;
                 case TimerChannel::Channel2:
-                    this->CCMR1 &= ~(0b11 << 8);  // Configure channel 2 in output compare mode (CC2S = 00 in TIMx_CCMR1)
-                    this->CCMR1 &= ~(0b111 << 12);// Clear output compare 2 mode configuration bits (OC2M)
+                    this->CCMR1 &= ~(0b11 << 8); // Configure channel 2 in output compare mode (CC2S = 00 in TIMx_CCMR1)
+                    this->CCMR1 &= ~(0b111 << 12); // Clear output compare 2 mode configuration bits (OC2M)
                     this->CCMR1 |= (0b110 << 12); // Set OC2M to PWM Mode 1 (0b110)
-                    this->CCMR1 |= (1 << 11);     // Enable Output Compare 2 Preload (OC2PE)
-                    this->CCER  |= (1 << 4);      // Enable Output Compare 2 output (CC2E in TIMx_CCER)
+                    this->CCMR1 |= (1 << 11); // Enable Output Compare 2 Preload (OC2PE)
+                    this->CCER |= (1 << 4); // Enable Output Compare 2 output (CC2E in TIMx_CCER)
                     break;
                 case TimerChannel::Channel3:
-                    this->CCMR2 &= ~(0b11 << 0);  // Configure channel 3 in output compare mode (CC3S = 00 in TIMx_CCMR2)
+                    this->CCMR2 &= ~(0b11 << 0); // Configure channel 3 in output compare mode (CC3S = 00 in TIMx_CCMR2)
                     this->CCMR2 &= ~(0b111 << 4); // Clear output compare 3 mode configuration bits (OC3M)
-                    this->CCMR2 |= (0b110 << 4);  // Set OC3M to PWM Mode 1 (0b110)
-                    this->CCMR2 |= (1 << 3);      // Enable Output Compare 3 Preload (OC3PE)
-                    this->CCER  |= (1 << 8);      // Enable Output Compare 3 output (CC3E in TIMx_CCER)
+                    this->CCMR2 |= (0b110 << 4); // Set OC3M to PWM Mode 1 (0b110)
+                    this->CCMR2 |= (1 << 3); // Enable Output Compare 3 Preload (OC3PE)
+                    this->CCER |= (1 << 8); // Enable Output Compare 3 output (CC3E in TIMx_CCER)
                     break;
                 case TimerChannel::Channel4:
-                    this->CCMR2 &= ~(0b11 << 8);  // Configure channel 4 in output compare mode (CC4S = 00 in TIMx_CCMR2)
-                    this->CCMR2 &= ~(0b111 << 12);// Clear output compare 4 mode configuration bits (OC4M)
+                    this->CCMR2 &= ~(0b11 << 8); // Configure channel 4 in output compare mode (CC4S = 00 in TIMx_CCMR2)
+                    this->CCMR2 &= ~(0b111 << 12); // Clear output compare 4 mode configuration bits (OC4M)
                     this->CCMR2 |= (0b110 << 12); // Set OC4M to PWM Mode 1 (0b110)
-                    this->CCMR2 |= (1 << 11);     // Enable Output Compare 4 Preload (OC4PE)
-                    this->CCER  |= (1 << 12);     // Enable Output Compare 4 output (CC4E in TIMx_CCER)
+                    this->CCMR2 |= (1 << 11); // Enable Output Compare 4 Preload (OC4PE)
+                    this->CCER |= (1 << 12); // Enable Output Compare 4 output (CC4E in TIMx_CCER)
                     break;
             }
         }
+
         /**
          * @brief Updates the duty cycle value for a specific timer channel.
          * @param channel The target timer channel (1 to 4).
@@ -169,10 +176,14 @@ namespace W2 {
          */
         void setDutyCycle(TimerChannel channel, unsigned int value) {
             switch (channel) {
-                case TimerChannel::Channel1: this->CCR1 = value; break;
-                case TimerChannel::Channel2: this->CCR2 = value; break;
-                case TimerChannel::Channel3: this->CCR3 = value; break;
-                case TimerChannel::Channel4: this->CCR4 = value; break;
+                case TimerChannel::Channel1: this->CCR1 = value;
+                    break;
+                case TimerChannel::Channel2: this->CCR2 = value;
+                    break;
+                case TimerChannel::Channel3: this->CCR3 = value;
+                    break;
+                case TimerChannel::Channel4: this->CCR4 = value;
+                    break;
             }
         }
 
@@ -288,17 +299,17 @@ namespace W2 {
     static_assert(sizeof(GPIO) == 0x1C);
 
 
-    /**
+    /***
      * @brief Universal Synchronous Asynchronous Receiver Transmitter (USART) peripheral memory map.
      * @details Mapped to physical memory offsets specified in Section 27.6 of the RM0008 Reference Manual.
      */
     struct USART {
-        volatile register_t SR;   ///< 0x00 USART Status Register (USART_SR).
-        volatile register_t DR;   ///< 0x04 USART Data Register (USART_DR).
-        volatile register_t BRR;  ///< 0x08 USART Baud Rate Register (USART_BRR).
-        volatile register_t CR1;  ///< 0x0C USART Control Register 1 (USART_CR1).
-        volatile register_t CR2;  ///< 0x10 USART Control Register 2 (USART_CR2).
-        volatile register_t CR3;  ///< 0x14 USART Control Register 3 (USART_CR3).
+        volatile register_t SR; ///< 0x00 USART Status Register (USART_SR).
+        volatile register_t DR; ///< 0x04 USART Data Register (USART_DR).
+        volatile register_t BRR; ///< 0x08 USART Baud Rate Register (USART_BRR).
+        volatile register_t CR1; ///< 0x0C USART Control Register 1 (USART_CR1).
+        volatile register_t CR2; ///< 0x10 USART Control Register 2 (USART_CR2).
+        volatile register_t CR3; ///< 0x14 USART Control Register 3 (USART_CR3).
         volatile register_t GTPR; ///< 0x18 USART Guard Time and Prescaler Register (USART_GTPR).
 
         /**
@@ -355,10 +366,96 @@ namespace W2 {
             return static_cast<uint8_t>(this->DR & 0xFF);
         }
     };
+
+    struct DMAChannel {
+        volatile register_t CCR; //!< DMA channel x configuration register
+        volatile register_t CNDTR; //!< DMA channel x number of data register
+        volatile register_t CPAR; //!< DMA channel x peripheral address register
+        volatile register_t CMAR; //!< DMA channel x memory address register
+        register_t RESERVED; //!< Reserved for alignment. DO NOT WRITE TO IT (13.4.7 Register Map)
+
+        enum class DMAPriorityLevel : unsigned char {
+            LOW = 0b00,
+            MEDIUM = 0b01,
+            HIGH = 0b10,
+            VERY_HIGH = 0b11
+        };
+        enum class DMAMemorySize : unsigned char {
+            BYTE = 0b00,
+            HALF_WORD = 0b01,
+            WORD = 0b10,
+        };
+        enum class TransferDirection : unsigned char {
+            FROM_PERIPHERAL_TO_MEMORY = 0b0,
+            FROM_MEMORY_TO_PERIPHERAL = 0b1,
+        };
+        bool isEnabled() const {
+            return CCR & 0b1;
+        }
+        void setPriorityLevel(DMAPriorityLevel level) {
+
+            constexpr int bit_start = 12;
+            // Clear last level
+            CCR &= ~(0b11 << bit_start);
+            // Set level
+            CCR |= (static_cast<register_t>(level) << bit_start);
+        }
+        void setSize(DMAMemorySize memory_size, DMAMemorySize peripheral_size) {
+            // set memory size
+            constexpr int m_bit_start = 10;
+            CCR &= ~(0b11 << m_bit_start);
+            // Set level
+            CCR |= (static_cast<register_t>(memory_size) << m_bit_start);
+
+            // set peripheral size
+            constexpr int p_bit_start = 8;
+            CCR &= ~(0b11 << p_bit_start);
+            // Set level
+            CCR |= (static_cast<register_t>(peripheral_size) << p_bit_start);
+        }
+        void setPeripheralAddress(const register_t* peripheral) {
+            if (isEnabled())
+                return;
+            CPAR = reinterpret_cast<register_t>(peripheral);
+        }
+        void setMemoryAddress(const uintptr_t* memory) {
+            if (isEnabled())
+                return;
+            CMAR = reinterpret_cast<register_t>(memory);
+        }
+        void setDataTransferMode(const TransferDirection dir ) {
+            constexpr unsigned int bit_start = 4;
+            CCR &= ~(0b1 << bit_start);
+            CCR |= (static_cast<register_t>(dir) << bit_start);
+        }
+        void enableTransferCompleteInterrupt(bool enabled) {
+            constexpr unsigned int bit_start = 1;
+            CCR &= ~(0b1 << bit_start);
+            CCR |= (static_cast<register_t>(enabled) << bit_start);
+        }
+    };
+
+    struct DMA1MemoryMap {
+        volatile register_t ISR;//!< DMA interrupt status register
+        volatile register_t IFCR;//!< DMA interrupt flag clear register
+
+        // Channel array (DMA1 has 7 channels)
+        DMAChannel CH[7];
+    };
+
+    struct DMA2MemoryMap {
+        volatile register_t ISR;//!< DMA interrupt status register
+        volatile register_t IFCR;//!< DMA interrupt flag clear register
+        // Channel array (DMA2 has 5 channels)
+        DMAChannel CH[5];
+    };
+
     // =========================================================================
     // Peripheral Base Addresses
     // =========================================================================
-    inline const auto USART1 = reinterpret_cast<USART*>(0x40013800u);
+    inline const auto DMA1 = reinterpret_cast<DMA1MemoryMap *>(0x4002'0000u);
+    inline const auto DMA2 = reinterpret_cast<DMA2MemoryMap *>(0x4002'0400u);
+    inline const auto USART1 = reinterpret_cast<USART *>(0x40013800u);
     /** @brief General Purpose Timer 2 on the APB1 Bus (TIM2 base address). */
     inline const auto TIMER2 = reinterpret_cast<TIMER *>(0x40000000u);
 
